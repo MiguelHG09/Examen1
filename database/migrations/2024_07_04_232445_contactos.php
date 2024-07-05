4a4888a0b90e309a75cf8bb1cfeb2baff91c8270
Miguel <miguelhg09@gmail.com>
2024-07-04 19:08:36 -0600

    Ultimos cambios

--- database/migrations/2024_07_04_232445_contactos.php
+++ database/migrations/2024_07_04_232445_contactos.php
@@ -10,9 +10,24 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    { Schema::create('directorio', function (Blueprint $table) {
+        $table->id('codigoEntrada');
+        $table->string('nombre');
+        $table->string('apellido');
+        $table->string('correo')-> unique;   
+        $table->string('telefono');
+     
+    });
+    { Schema::create('contactos', function (Blueprint $table) {
+        $table->id();
+        $table->string('codigoEntrada') -> primaryKey;
+        $table->string('nombre');
+        $table->string('apellido');
+        $table->string('telefono');
+     
+    });
         //
-    }
+    }}
 
     /**
      * Reverse the migrations.
